6c4a2aab9b64ccafe74310a5f56f5dc80e94df35
PablooEs <pablo_j_es@hotmail.com>
2022-03-07 16:44:47 -0300

    init

---
+++ src/main/java/com/pablo/TextAnalisisApplication.java
@@ -0,0 +1,13 @@
+package com.pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalisisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalisisApplication.class, args);
+	}
+
+}
