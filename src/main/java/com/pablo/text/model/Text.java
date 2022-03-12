c00f93513007059173a1c139cc2a5466d82ce1d7
PablooEs <pablo_j_es@hotmail.com>
2022-03-12 16:30:08 -0300

    Ultimos detalles

--- src/main/java/com/pablo/text/model/Text.java
+++ src/main/java/com/pablo/text/model/Text.java
@@ -1,13 +1,8 @@
 package com.pablo.text.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
--- src/test/java/com/pablo/TextAnalisisApplicationTests.java
+++ src/test/java/com/pablo/TextAnalisisApplicationTests.java
@@ -3,7 +3,9 @@ package com.pablo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.pablo.text.TextAnalisisApplication;
+
+@SpringBootTest(classes = TextAnalisisApplication.class)
 class TextAnalisisApplicationTests {
 
 	@Test
